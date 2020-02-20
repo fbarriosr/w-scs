1e4eef2e4c4bee04782a76197ef4598fc1ff3d14
pancho <francisco.barriosr@gmail.com>
2020-02-20 20:51:04 -0300

    mini web proyecto

---
+++ footer.php
@@ -0,0 +1,25 @@
+
+  <footer>
+    <div class="container">
+      <p>ELSO CLC All Rights Reserved.</p>
+      <ul class="list-inline">
+        <li class="list-inline-item">
+          <a href="./privacy.php">Privacy</a>
+        </li>
+      </ul>
+    </div>
+  </footer>
+
+  <!-- Bootstrap core JavaScript -->
+  <script src="vendor/jquery/jquery.min.js"></script>
+  <script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+
+  <!-- Plugin JavaScript -->
+  <script src="vendor/jquery-easing/jquery.easing.min.js"></script>
+
+  <!-- Custom scripts for this template -->
+  <script src="js/new-age.min.js"></script>
+
+</body>
+
+</html>
---
+++ privacy.php
@@ -0,0 +1,65 @@
+<?php include("header-privacy.php"); ?>
+
+
+<section class="download overlay text-center" id="download">
+    <div class="container ">
+      <div class="row">
+        <div class="col-md-8 mx-auto pol">
+         
+        </div>
+      </div>
+    </div>
+    
+</section>
+
+<section class=" bg-primary text-justify color-secundary">
+  <div class="container">
+      <div class="row">
+        <div class="col-md-8 mx-auto pol">
+            <h2>Privacy</h2>
+            <h4>Collection and processing of personal data</h4>
+            <p>Personal data are those that can be used to identify or contact a person.</p>
+            <p>ELSO CLC may request personal data of users when accessing applications of the company or other affiliated companies as well as the possibility that among these companies can share that data to improve the products and services offered. If these personal data are not provided, in many cases we will not be able to offer the products or services requested.</p>
+
+            <p>These are some examples of the categories of personal data that ELSO CLC may collect and the purpose for which it may carry out the processing of this data.</p>
+
+            <p>What personal data can be collected?</p>
+            <ul>
+              <li>Personal email address can be collected?</li>
+            </ul>
+
+            <p>Purpose of the processing of personal data</p>
+
+            <p>ELSO CLC may use the personal data collected to:</p>
+
+            <ul>
+              <li>The personal data collected allows you to keep informed about the latest products, available software updates and upcoming events.</li>
+              <li>Personal data is also used as an aid to elaborate, improve, manage, provide and improve products, services, content and advertising, and with the purpose of avoiding losses and fraud.</li>
+              <li>Personal data can be used to verify identity, collaborate in the identification of users and decide on the appropriate services.</li>
+              <li>This personal data is also used for internal purposes, including audits, data analysis and surveys, to improve products, services and customer communications.</li>
+              <li>If you participate in a raffle, contest or promotion, the data provided can be used to administer these programs.</li>
+
+            </ul>          
+            
+
+            <h4>Disclosure to third parties</h4>
+            <p>The company ELSO CLC will only disclose personal data by legal mandate, within the framework of a judicial process or at the request of a public authority, both inside and outside your country of residence. Likewise, personal information may be published if necessary or convenient for national security reasons, to comply with current legislation or for other relevant public order reasons.</p>
+
+            <p>ELSO CLC will guarantee the protection of personal data by encryption during transit and, those housed in facilities, with physical security measures.</p>
+
+            <p>When using certain products, services or applications or when posting opinions on forums, chat rooms or social networks, the content and personal data that is shared will be visible to other users, who will have the possibility to read, compile or use them. You will be responsible for the personal data that you distribute or provide in these cases.</p>
+
+            <h4>Integrity and conservation of personal data</h4>
+
+            <p>ELSO CLC will guarantee the accuracy and quality of personal data, they will be kept for the time necessary to fulfill the purposes for which they were collected, unless the law requires them to be kept for longer.</p>
+            <h4>Access to personal data</h4>
+            <p>Regarding the personal data that we keep, ELSO CLC offers you access to them for any purpose, including requests for rectification in case they are incorrect or for deletion in case you are not obliged to keep them for legal reasons or for legitimate reasons of deal. We reserve the right not to process those requests that are inadmissible or vexatious, that jeopardize the privacy of third parties, that are unfeasible or for which local legislation does not require the right of access. Requests for access, rectification or deletion may be sent to our email account rdiaz@anestesiologia.cl </p>
+  
+        </div>
+      </div>
+    </div>
+    
+</section>
+
+  
+<?php include("footer.php"); ?>
